df9cfbf7d26b31b7c2f27889ce01e4f48a50fc2c
Gean20 <i201812633@cibertec.edu.pe>
2024-08-01 00:10:52 -0500

    ventana lista

--- Assets/Scripts/General/FlujoJuego/Botones.cs
+++ Assets/Scripts/General/FlujoJuego/Botones.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Botones : MonoBehaviour
 {
+    public GameObject PantallaActivado;
+    public GameObject PopUpButtonI;
     public void Reiniciar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ActivarPopUp()
+    {
+        if (!PantallaActivado.activeSelf)
+        {
+            PantallaActivado.SetActive(true);
+            PopUpButtonI.SetActive(false);
+        }
+    }
+    public void CerrarPopUp()
+    {
+        if (PantallaActivado.activeSelf)
+        {
+            PantallaActivado.SetActive(false);
+            PopUpButtonI.SetActive(true);
+        }
     }
 
 }
